340048a2352cd05d89e75294032e5c5c787ddecd
tjsjp <iwai@tjs.jp>
2025-10-31 08:47:16 +0900

    Update schedule-grid.js

--- schedule-grid.js
+++ schedule-grid.js
@@ -108,8 +108,8 @@
     };
     // Columns: 2 frozen meta columns + employees
     const columns = [
-      { title: meta.labels.date, type: 'text', readOnly: true, width: 35 },
-      { title: meta.labels.slot, type: 'text', readOnly: true, width: 40 },
+      { title: meta.labels.date, type: 'text', readOnly: true, width: (opts?.isProbablyMobile ? 30 : 35)},
+      { title: meta.labels.slot, type: 'text', readOnly: true, width: (opts?.isProbablyMobile ? 30 : 35)},
       ...meta.employees.map((name) => ({
         title: name,
         type: 'text',
@@ -237,6 +237,5 @@
     window.GridInstance = grid;
     return grid;
   }
-
   window.ScheduleGrid = { init };
 })();
